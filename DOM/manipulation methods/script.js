2b44d1736484e558ae28ba7f84d3f3cea707d9ac
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-10-08 17:06:42 +0600

    manipulation methods

---
+++ DOM/manipulation methods/script.js
@@ -0,0 +1,17 @@
+var divToManipulate = document.querySelector('.background');
+/* 
+divToManipulate.style.height = '50px';
+divToManipulate.style.width = '400px';
+divToManipulate.style.backgroundColor = 'red';
+divToManipulate.style.fontSize = '30px';
+divToManipulate.style.border = '5px solid black';
+*/
+
+/*
+divToManipulate.classList.add('addClass');
+divToManipulate.classList.remove('removeClass');
+
+*/
+
+divToManipulate.classList.toggle('toggleClass');
+
