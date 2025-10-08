169beb5c7dce1fb9e87573aee7a89c07aa47dbd9
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-10-08 14:15:10 +0600

    added files

---
+++ DOM/Select methods/script.js
@@ -0,0 +1,20 @@
+var SelectWithId = document.getElementById('idkparagraph');
+console.log(SelectWithId);
+
+var SelectWithClass = document.getElementsByClassName('background');
+console.log(SelectWithClass);
+
+var SelectWithTag = document.getElementsByTagName('h1');
+console.log(SelectWithTag);
+
+var SelectWithQuery = document.querySelector('h1');
+console.log(SelectWithQuery);
+
+var SelectWithQueryId = document.querySelector('#idkparagraph');
+console.log(SelectWithQueryId);
+
+var SelectWithqueryClass = document.querySelector('.background');
+console.log(SelectWithqueryClass);
+
+var SelectWithQueryAllClass = document.querySelectorAll('.background');
+console.log(SelectWithQueryAllClass);
